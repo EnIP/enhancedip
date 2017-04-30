6f72a378b026f2afde5e7722fde4a58dc7e42709
sam <sam@enhancedip.org>
2017-04-29 22:52:12 -0400

    updates to documentation

--- v1.3/kernel/4.4.6/eipnat/nat_N1.sh
+++ v1.3/kernel/4.4.6/eipnat/nat_N1.sh
@@ -20,6 +20,6 @@ iptables -t nat -A POSTROUTING -d 2.2.2.0/24 -o eth0 -j MASQUERADE
 iptables -t nat -A POSTROUTING -o eth2 -j MASQUERADE
 
 #load Enhanced IP NAT driver
-insmod /root/enhancedip-code/kernel/2.6.38/eipnat/eipnat.ko masq_ip=$MASQ_IP nic_name=$NIC_NAME
+insmod eipnat.ko masq_ip=$MASQ_IP nic_name=$NIC_NAME
 
 
